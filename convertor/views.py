3f00c6f9d3baa81f3374f77088519e63d6a4c749
Caliber24 <amirhosseinpishroa2001@gmail.com>
2025-02-28 19:04:43 +0330

    temporary upload file and optimize requirements

--- convertor/views.py
+++ convertor/views.py
@@ -1,17 +1,16 @@
 import os
 from email.policy import default
-
-import boto3
-from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins, generics
+import tempfile
+from rest_framework.viewsets import GenericViewSet, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import VTTSerializer
 from .models import VTT
 import uuid
-from botocore.client import Config
 from django.core.files.storage import default_storage
 from .convertor import transcribe_audio
+from django.core.files.uploadedfile import UploadedFile
 from django.conf import settings
 import os
 
@@ -29,19 +28,14 @@ class VTTViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin,
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        audio_file = serializer.validated_data['audio']
+        audio_file: UploadedFile = serializer.validated_data['audio']
         print(audio_file)
-        file_name = f'{uuid.uuid4()}{os.path.splitext(audio_file.name)[1]}'
-        s3 = boto3.client('s3', endpoint_url=LIARA['endpoint'], aws_access_key_id=LIARA['accesskey'],
-                          aws_secret_access_key=LIARA['secretkey'],
-                          config=Config(signature_version='s3v4', s3={'addressing_style': 'path'}, region_name='default'))
-        try:
-            with audio_file.open('rb') as audio_file_open:
-                s3.upload_fileobj(audio_file_open, LIARA['bucket'], file_name)
-                
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        file_path = f'{LIARA["endpoint"]}/{LIARA["bucket"]}/{file_name}'
-        transcription = transcribe_audio(file_path)
-        vtt_instance = serializer.save(user=request.user, audio=file_name, transcript=transcription)
+        
+        with tempfile.NamedTemporaryFile(suffix=os.path.splitext(audio_file.name)[1], delete=False) as tmp:
+            for chunk in audio_file.chunks():
+                tmp.write(chunk)
+                tmp.flush()
+            transcription = transcribe_audio(tmp.name)     
+            vtt_instance = serializer.save(user=request.user, audio=audio_file, transcript=transcription)
+            os.remove(tmp.name)
         return Response(VTTSerializer(vtt_instance).data, status=status.HTTP_201_CREATED)
